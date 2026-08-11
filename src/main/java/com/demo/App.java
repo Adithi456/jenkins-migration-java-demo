9247ea7f6f03d54a1cf6df71c9888abca1b14092
Adithi S Renke <adithirenake456@gmail.com>
2026-08-11 11:16:05 +0530

    Add initial Java application with greeting message

---
+++ src/main/java/com/demo/App.java
@@ -0,0 +1,12 @@
+package com.demo;
+
+public class App {
+
+    public static String getMessage() {
+        return "Hello from Java Demo";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getMessage());
+    }
+}
